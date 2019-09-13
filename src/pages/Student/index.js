4d2fef26e8c5e7da7448d26ba20c94857cfac431
unknown <fernando.ctech@gmail.com>
2019-09-13 19:30:59 -0300

    aqui vais

--- src/pages/Student/index.js
+++ src/pages/Student/index.js
@@ -1,11 +1,47 @@
 import React, {Component} from  'react';
+import api from '../../services/api';
 import './styles.css';
 
 export default class Student extends Component {
-  render() {
-    return (
-    <h2>Hello Student</h2>
-  );
+  state = {
+    authors: [],
+
+  }
+  componentDidMount() {
+    this.loadPosts();
   };
-  
-}
+  loadPosts = async () => {
+    const response = await api.get(`/authors`);
+
+    this.setState({ authors: response.data })
+    console.log(response);
+  };
+
+    render() {
+
+    return (
+      
+    <div className="product-list">
+        <div className="info">
+          <h1>EQUIPE</h1>
+          <h3>NutriExpress</h3>
+        </div>
+        {this.state.authors.map(author => (
+          <div className="content">
+            <div className="card-body">
+              <div className="card-image">
+                <img src={author.image_perfil} />
+              </div>
+              <div className="car-text">
+                <strong>{author.name}</strong>
+                <p>: Estudante de Nutrição</p> 
+                
+              </div>
+            </div>
+
+          </div>
+      ))};
+      </div>
+    )
+        }
+      }
