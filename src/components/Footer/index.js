63ec3761ca995a1abf1538ebf411bd45cf553223
unknown <fernando.ctech@gmail.com>
2019-09-13 20:05:37 -0300

    nando

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,20 @@ import './styles.css';
 
 const Footer = () => (
   <footer id="main-footer" >
-    <div><h1>NutriExpress</h1></div>
+    <div className="content">
+      <div className="card-body">
+        <div className="card-image">
+          <img src="https://lh3.googleusercontent.com/XgBsri3Rd3NSSbJkaea9cpnYUIGphPerE_ike7DTbBtMQX4Al1PE_hHtL7JbRA_DE1qFyLynWYUu97tY2dpztr9L2FV6aSwH1yjxfii1NvJ2_FUJO1govzNcuoCmwXGqTu45uyAEJXGkLwtwSiCmDGJmHlNSDrQODnieMf__UD4hIcMEzl0BIeMUpxDvZSRhRNzAvM0xN4rgr-yjBgT6wxHWjsZ3i_TabWIoDbHapACk5_ejQzOggTUwygqB8Qzi_xIKw0RCg8wSwm19ED21Gl_cqLD7vE_7YlfIxx2rxS9MkYaf2VtiEI3CMZsmCz9XGp3aTmTZlmpadY5OzInIsZ4lk8bJzm2qgNopinIIusb_OsBgahcbp5WEkMOPomFWGAmtXnb1EZVMOHM1WhYfC-pBOwYXnlVzlKyheMiMlSQc6iCU4uyrjrjpX5OSyqc7gZt8ac31l3KrMzDnv6fljyNkTU0LwINeAOzbCXhJt0luP9af0H7zOvSfmSbznduCobK7-qtSNGrQVM0sYtIXaCUgQVgL-BEPVedGb8bjlnxr_2fohact-Vi8V1EwvzNZTSyS1qDRN7QT1OjRPzH7zD6IABLJrizo5iWtkHwlHtX4yrw7tDV0BRwdeyu5s_rJGAdCtmWmKsSyNsXj-bRqO2vh1u0siHpM6inxuz6mDFT3DRPbbWXjW6s=w362-h363-no" />
+        </div>
+        <div className="car-text">
+          <strong>Fernando</strong>
+          <p>Desenvolverdor</p>
+          <p>Designer Web</p>
+
+        </div>
+      </div>
+
+    </div>
   </footer>
 );
 
