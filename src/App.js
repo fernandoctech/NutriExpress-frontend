f5ed0715902d122600360fd10465ed7353c20124
unknown <fernando.ctech@gmail.com>
2019-09-13 12:50:18 -0300

    mudanças

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import './styles.css';
 import Route from'./routes';
+
 
 const App = () => (
   <div className="App" >
     <Header/>
     <Route/>
+    <Footer/>
     
   </div>
 );
---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const Footer = () => (
+  <footer id="main-footer" >
+    <div><h1>NutriExpress</h1></div>
+  </footer>
+);
+
+export default Footer ;
